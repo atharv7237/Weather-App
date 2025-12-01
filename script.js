931c8267d60c7ffc2454265a4db30e8a49984255
Atharv sapale <atharvsapale90@gmail.com>
2025-12-01 20:20:10 +0530

    Fix temperature conversion and clean up fetch URL in getTemp function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 async function getTemp(){
   
     let cityName = document.getElementById("cityName").value;
-    let fetched = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4f116c5cdcde209f70a585124b5a134f&units=metric //metric for celsius
+    let fetched = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4f116c5cdcde209f70a585124b5a134f&units=metric 
 `)
     let data = await fetched.json();
     console.log(data); // just for checking in console
     try
     {
-    document.getElementById("temprature").innerText = Math.floor((data.main.temp)-273.15) + "C"; // Kelvin to Celsius
+    document.getElementById("temprature").innerText = (data.main.temp) + "C"; // Kelvin to Celsius
     }
     catch(err)
     {
