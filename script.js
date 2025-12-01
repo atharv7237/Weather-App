53d3d84b65bf095f7c8fc472c418d51b26821c4c
Atharv sapale <atharvsapale90@gmail.com>
2025-12-01 20:36:19 +0530

    Fix API key in fetch URL for getTemp function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function getTemp(){
   
     let cityName = document.getElementById("cityName").value;
-    let fetched = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4f116c5cdcde209f70a585124b5a134f&units=metric 
+    let fetched = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=Your_api_keyf&units=metric 
 `)
     let data = await fetched.json();
     console.log(data); // just for checking in console
